3bded9d3a10711daaea75528811aedbe26d0143e
fatihsaracoglu <fatihsaracoglu@outlook.com>
2019-08-24 22:53:16 +0300

    Edited chart title and rearranged component file

--- src/main/java/tr/gov/tubitak/bilgem/yte/alerting/view/src/components/AlertDetail.js
+++ src/main/java/tr/gov/tubitak/bilgem/yte/alerting/view/src/components/AlertDetail.js
@@ -8,28 +8,53 @@ var dps = [];
 var xVal = 0;
 var yVal = 0;
 
+const options = {
+    title: {
+        text: "Status: Connection waiting...",
+        fontColor: "#F4BD24",
+        fontWeight: "normal",
+        fontSize: 20,
+    },
+    axisX: {
+        title: "Time (s)",
+        lineThickness: 2,
+        lineColor: "#F4BD24",
+        labelFontColor: "#F4BD24",
+        titleFontColor: "#F4BD24"
+    },
+    axisY: {
+        title: "Status",
+        lineThickness: 2,
+        lineColor: "#F4BD24",
+        labelFontColor: "#F4BD24",
+        titleFontColor: "#F4BD24"
+    },
+    data: [{
+        type: "line",
+        dataPoints: dps,
+        lineColor: "white",
+        markerColor: "#F4BD24",
+    }],
+    backgroundColor: "#161b21"
+};
+
 class AlertDetail extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: '',
             url: '',
-            method: '',
             period: '',
             status: '',
         };
         this.updateChart = this.updateChart.bind(this);
         this.fetchAlerts();
-        // this.goBack = this.goBack.bind(this);
     }
 
     fetchAlerts = () => {
         axios.get("http://localhost:8080/alerts/" + this.props.match.params.id)
             .then(res => {
                 this.setState({
-                    name: res.data.name,
                     url: res.data.url,
-                    method: res.data.method,
                     period: res.data.period,
                     status: res.data.alertDetailSet[res.data.alertDetailSet.length - 1]
                 });
@@ -51,6 +76,13 @@ class AlertDetail extends Component {
     updateChart() {
         yVal = this.state.status.status;
         dps.push({x: xVal, y: yVal});
+        if (yVal === 1) {
+            options.title.text = "Status of " + this.state.url + ": Connection OK!";
+            options.title.fontColor = "#98FB98";
+        } else {
+            options.title.text = "Status of " + this.state.url + ": Connection FAILED!";
+            options.title.fontColor = "#DC143C";
+        }
         xVal = xVal + this.state.period;
         if (dps.length > 10) {
             dps.shift();
@@ -58,42 +90,7 @@ class AlertDetail extends Component {
         this.chart.render();
     }
 
-    // goBack() {
-    //     this.props.history.goBack();
-    // }
-
-
     render() {
-        const options = {
-            title: {
-                text: "Website Status:  " + this.state.url,
-                fontColor: "#F4BD24",
-                fontWeight: "normal",
-                fontSize: 20,
-            },
-            axisX: {
-                title: "Time (s)",
-                lineThickness: 2,
-                lineColor: "#F4BD24",
-                labelFontColor: "#F4BD24",
-                titleFontColor: "#F4BD24"
-            },
-            axisY: {
-                title: "Status",
-                lineThickness: 2,
-                lineColor: "#F4BD24",
-                labelFontColor: "#F4BD24",
-                titleFontColor: "#F4BD24"
-            },
-            data: [{
-                type: "line",
-                dataPoints: dps,
-                lineColor: "white",
-                markerColor: "#F4BD24",
-            }],
-            backgroundColor: "#161b21"
-        };
-
         return (
             <div className="AlertDetail">
                 <a href="/alerts"><Button variant="warning" className="back-btn previous round">&#8249;</Button></a>
